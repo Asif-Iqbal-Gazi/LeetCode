bc3a4ec75b682432212aab08b2abf389e92b0fdb
Asif Iqbal Gazi <20272104+Asif-Iqbal-Gazi@users.noreply.github.com>
2023-07-23 09:30:54 -0500

    Signed Commit??

---
+++ Unknown/easy/string/58.length_of_last_word.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=58 lang=cpp
+ *
+ * [58] Length of Last Word
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        /* Approach:
+            1. Linear Scan -- T.C: O(n), S.C: O(1)
+        */
+
+        int l = 0, r = 0, n = s.size();
+        // Remove all leading and extra zeros in between
+        while (r < n)
+        {
+            if (s[r] != ' ')
+                s[l++] = s[r++];
+            else if (r > 0 && s[r - 1] != ' ')
+                s[l++] = s[r++];
+            else
+                r++;
+        }
+        // Figureout how many chars to delete from left
+        // Check if we have a trailing zero
+        if (s[l - 1] == ' ')
+            l--;
+        int noOfCharsToRemove = n - l + 1;
+
+        while (--noOfCharsToRemove)
+        {
+            s.pop_back();
+        }
+
+        n = s.size();
+        r = n - 1;
+        l = r;
+
+        while (l >= 0 && s[l] != ' ')
+            l--;
+
+        return r - l;
+    }
+};
+// @lc code=end
