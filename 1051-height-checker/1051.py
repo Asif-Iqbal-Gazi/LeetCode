5dcffe682e4312ab2c1db45f78c294783dab7fd9
Asif Iqbal Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2026-04-01 12:23:11 -0700

    Update README 1051

--- 1051-height-checker/1051.py
+++ 1051-height-checker/1051.py
@@ -1,9 +1,11 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
+        # Approach 2: Counting Sort
+        #               T.C: O(N), S.C: O(1)
         count = [0] * 101
         for h in heights:
             count[h] += 1
-    
+
         res = 0
         curr_height = 1
         for h in heights:
@@ -13,3 +15,15 @@ class Solution:
                 res += 1
             count[curr_height] -= 1
         return res
+
+        """
+        # Appraoch 1: Sorting
+        #           T.C: O(n log n), S.C: O(n)
+        sorted_heights = sorted(heights)
+
+        res = 0
+        for i in range(0, len(heights)):
+            if sorted_heights[i] != heights[i]:
+                res += 1
+        return res
+        """
